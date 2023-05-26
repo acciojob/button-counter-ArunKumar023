afbec71af8eb5d3eca8f1fa38371ccc9084ad545
Arun kumar <ak2965491@gmail.com>
2023-05-26 15:29:20 +0530

    submitting code-Fri May 26 2023 15:29:07 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,16 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-import {useState} from 'react'
 
 const App = () => {
-  let [count,setCount]=useState(0)
-  function counter(){
-    setCount(count+1)
-  }
+  const [count, setCount] = useState(0);
   return (
     <div>
       <p>Button clicked {count} times</p>
-        <button onClick={counter}>Click me</button>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   )
 }
 
 export default App
-
